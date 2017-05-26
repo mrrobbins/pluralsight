1db64d3d4f4ec2264f649dea8a2086d96b3798db
Matthew Robbins <matthew.r.robbins@jpmchase.com>
2017-05-26 14:58:56 -0400

    Added knotlin-getting-started

---
+++ ps-kotlin-getting-started/src/hello.kt
@@ -0,0 +1,13 @@
+/*
+ * Date: 5/26/17 1:07 PM
+ *
+ * The source code contained in this listing is proprietary to JPMorgan Chase, Inc.
+ *
+ * Unauthorized copying, adaptation, distribution, use, or display is strictly prohibited.
+ * This software is Copyright 2017 JPMorgan Chase, Inc.
+ */
+
+fun main(args: Array<String>) {
+    println("hello world")
+}
+
