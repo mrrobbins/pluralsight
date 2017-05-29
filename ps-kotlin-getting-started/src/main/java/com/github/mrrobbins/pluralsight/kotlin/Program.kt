7c86de53a48f5c0811d5ec651c527ff91924d2ac
Matt <matthew.r.robbins@gmail.com>
2017-05-29 15:41:52 -0400

    Added examples from Functional Programming section.

--- ps-kotlin-getting-started/src/main/java/com/github/mrrobbins/pluralsight/kotlin/Program.kt
+++ ps-kotlin-getting-started/src/main/java/com/github/mrrobbins/pluralsight/kotlin/Program.kt
@@ -2,16 +2,31 @@ import com.github.mrrobbins.pluralsight.kotlin.provider.Providers
 import java.security.Provider
 
 fun main(args: Array<String>) {
-    val programmer = "Matt";
+    val programmer = "Matt"
     println("hello $programmer! Let's run some kotlin code...\n")
 
     listProviders(Providers().getProviders()) // instance method call
 
-    listProviders(Providers.getProviders()); // companion object method call
+    listProviders(Providers.getProviders()) // companion object method call
+
+    // using biconsumer lambda
+    listAllProviders {
+        key, value ->
+        println("\t---- $key: $value")
+    }
 }
 
 private fun listProviders(providers: List<Provider>) {
     println("Java Security Providers:")
     providers.forEach({ provider -> println(provider.name); provider.forEach({ key, value -> println("\t $key: $value") }) })
+}
+
+fun listAllProviders(fn: (String, String) -> Unit) {
+    val allProviders = Providers.getProviders()
+
+    for (provider in allProviders) {
+        println(provider.name)
+        provider.forEach { key, value -> fn(key.toString(), value.toString()) }
+    }
 }
 
--- ps-kotlin-getting-started/src/main/java/com/github/mrrobbins/pluralsight/kotlin/provider/Providers.kt
+++ ps-kotlin-getting-started/src/main/java/com/github/mrrobbins/pluralsight/kotlin/provider/Providers.kt
@@ -2,13 +2,46 @@ package com.github.mrrobbins.pluralsight.kotlin.provider
 
 import java.security.Security
 
+data class ProviderDetails(val providerName: String, val name: String)
 
 class Providers {
 
     fun getProviders() = Security.getProviders().toList();
 
+    fun getAllProviderDetails(filter: String): List<ProviderDetails> {
+        return Security.getProviders().flatMap {
+            provider ->
+            provider.entries
+                    .filter { it.key.toString().contains(filter, true) }
+                    .map { ProviderDetails(provider.name, it.key.toString()) }
+        }
+
+    }
+
+    fun getAllProviderDetailsFirstAttempt(filter: String): List<ProviderDetails> {
+        val providers = Security.getProviders();
+        val listOfProviders = mutableListOf<ProviderDetails>()
+
+        providers.forEach {
+            provider ->
+            val providerDetailsList = provider.entries
+                    .filter { it.key.toString().contains(filter, true) }
+                    .map { ProviderDetails(provider.name, it.key.toString()) }
+            listOfProviders += providerDetailsList
+        }
+
+        return listOfProviders
+    }
+
     // similar convention to java static methods
     companion object {
         fun getProviders() = Security.getProviders().toList();
     }
+}
+
+fun main(args: Array<String>) {
+    val providers = Providers()
+    val details = providers.getAllProviderDetails("Random")
+
+    details.forEach { println(it) }
 }
